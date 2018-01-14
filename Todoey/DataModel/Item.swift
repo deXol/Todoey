9a4206fe2c47e6222f68078ac9f4f2bd0626a33e
Olivér Tóth <oli92facebook@gmail.com>
2018-01-14 21:15:55 +0100

    Convert to MVC design pattern

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Toth Oliver on 1/14/18.
+//  Copyright © 2018 deXol. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
